5540a256bbf44f1a540b4bd23dec5bc5166271ee
feroxx <frhatfrat@gmail.com>
2016-03-22 17:47:46 +0200

    Create cm.mk

---
+++ cm.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2013 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# exclude live wallpapers
+TARGET_EXCLUDE_LIVEWALLPAPERS := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/aries
+TARGET_KERNEL_CONFIG := omni_p1_defconfig
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# bootanimation
+TARGET_BOOTANIMATION_SIZE := 640x362
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration.
+$(call inherit-product, device/samsung/p1/device.mk)
+
+# --------------------------------------------------------------------------------
+# Discard inherited values and use our own instead.
+# --------------------------------------------------------------------------------
+PRODUCT_NAME := cm_p1
+PRODUCT_DEVICE := p1
+PRODUCT_MODEL := GT-P1000
